e5871b0163cfc7c13bdc7497c3c5dca95d939a2f
hado anselme <anselmehado85@yahoo.com>
2021-06-22 18:54:54 +0100

    fonctionsup

--- app/controllers/agendas_controller1.rb
+++ app/controllers/agendas_controller1.rb
@@ -15,23 +15,11 @@ class AgendasController < ApplicationController
     @agenda.team = Team.friendly.find(params[:team_id])
     current_user.keep_team_id = @agenda.team.id
     if current_user.save && @agenda.save
-      redirect_to dashboard_url, notice: I18n.t('views.messages.create_agenda')
+      redirect_to dashboard_url, notice: I18n.t('views.messages.create_agenda') 
     else
       render :new
     end
   end
-
-  def destroy
-    @agenda = Agenda.find(params[:id])
-    if Team.owner = current_user
-      @agenda.destroy
-      
-
-    end
-
-  end
-
-
 
   private
 
